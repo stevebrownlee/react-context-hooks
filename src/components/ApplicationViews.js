c31ddbd4734ee24926e962bddfbf4b772fdedecb
Steve Brownlee <chortlehoort@gmail.com>
2019-06-16 11:56:28 -0500

    Higher order component for providers This allows me to continue to have multiple contexts that are specialized to specific resources, but I can wrap all of my data-dependent Routes in a single Provider component

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom"
 import React, { Component } from "react"
+import { KennelProvider } from "../data/KennelProvider";
 
 import Login from "./auth/Login"
 import AuthRoute from "./auth/AuthRoute"
@@ -8,29 +9,27 @@ import SearchResults from "./search/SearchResults"
 
 import AnimalList from "./animals/AnimalList"
 import Animal from "./animals/Animal"
-import { AnimalProvider } from "../data/AnimalProvider"
 
 import LocationList from "./locations/LocationList"
 import Location from "./locations/Location"
-import { LocationProvider } from "../data/LocationProvider"
+
 
 
 export default class ApplicationViews extends Component {
     render() {
         return (
             <React.Fragment>
-                <AnimalProvider>
+                <KennelProvider>
                     <Route path="/animals" component={AnimalList} />
                     <Route path="/animals/:animalId" render={(props) => {
                         return <Animal {...props} />
                     }} />
-                    <LocationProvider>
-                        <Route path="/locations" component={LocationList} />
-                        <Route path="/locations/:locationId" render={(props) => {
-                            return <Location {...props} />
-                        }} />
-                    </LocationProvider>
-                </AnimalProvider>
+
+                    <Route path="/locations" component={LocationList} />
+                    <Route path="/locations/:locationId" render={(props) => {
+                        return <Location {...props} />
+                    }} />
+                </KennelProvider>
 
 
                 <Route path="/login" component={Login} />
--- src/data/KennelProvider.js
+++ src/data/KennelProvider.js
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { fetchIt } from './Fetch';
+import React from 'react';
 
-// The context is imported and used by individual components that need data
-export const KennelContext = React.createContext([{}, () => {}])
+import { LocationProvider } from "../data/LocationProvider"
+import { AnimalProvider } from "../data/AnimalProvider"
 
-
-/*
- This component establishes what data can be used.
- */
 export const KennelProvider = props => {
-    const [animals, setAnimals] = useState([]);
-
-    useEffect(() => {
-        fetchIt("http://localhost:5002/animals").then(setAnimals)
-    }, [])
 
     return (
-        <KennelContext.Provider value={{animals, setAnimals}}>
-            {props.children}
-        </KennelContext.Provider>
+        <AnimalProvider>
+            <LocationProvider>
+                {props.children}
+            </LocationProvider>
+        </AnimalProvider>
     )
 }
 
